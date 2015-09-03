03c38da5db28ea972eff8c5d47e742f9438ea6f2
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-09-03 17:11:42 +0000

    Task 2 added

--- task_02.py
+++ task_02.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print "Hello Student!"
+print "It's amazing how nice people are in the Python development world. Just as with any community, however, it's important to follow certain rules of decorum.
+"
