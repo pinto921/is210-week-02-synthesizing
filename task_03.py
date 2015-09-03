fd7346c7b9ad34e0bf5cdf29337d803916f3f0aa
Shinseeker <dailymanj88@live.com>
2015-09-03 16:25:44 +0000

    task_03

--- task_03.py
+++ task_03.py
@@ -1,4 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Just added a docstring"""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
