7ca93eaaaf0d89b3887f6dd3a0983d71ab58df40
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-09-03 18:16:27 +0000

    task_03.py

--- task_03.py
+++ task_03.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+CHARLIE = "Chocolate Factory"
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-CHARLIE = "Chocolate Factory" 
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
+CHARLIE = "Brown"
 
 
