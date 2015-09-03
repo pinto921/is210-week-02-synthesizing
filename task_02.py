47dadd6982ac09466ae182fdf2560b450bbafe0d
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-09-03 17:59:07 +0000

    task 2 added

--- task_02.py
+++ task_02.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-("""Python syntax is really quite simple to execute. Anyone can do it just by executing the python command on the command line and invoking the interpreter.""")
+"""This is a docstring"""
+
+
 print "Hello Student!"
