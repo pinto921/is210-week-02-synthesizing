4cba22dd1c078804e002a50b0b391aa55126c976
Saulat Majid <solyraja@hotmail.com>
2015-09-04 22:36:34 +0000

    Adding all 4 files.

--- task_02.py
+++ task_02.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""A simple greeting for an IS-210 student"""
+
 
 print "Hello Student!"
--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the person who owns a huge place to make sweet treats."""
+
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
