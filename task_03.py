82bde7b72bcecd0050d0388b5b5c8e9b90436d19
Farzad Hejazi <fhejazi@gmail.com>
2015-09-04 04:06:02 +0000

    Added shebang, encoding and docstring lines.

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the docstring"""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
