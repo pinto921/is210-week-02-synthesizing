8c0607ddbd2017bb23fa4419ae4c9413bdc5e2ec
Nick Ileczko <nickileczko@gmail.com>
2015-09-05 17:27:20 +0000

    Corrected file changed Chocolate Factory to Brown

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A file that stores variables possibly?."""
+
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
