a97457f9dd580ad5a846acd89494a6babe70bc2f
Alex Doler <aedoler@gmail.com>
2015-09-07 00:40:37 +0000

    ran tests

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This the docstring. Tests are run on this file
+fix the python code."""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
