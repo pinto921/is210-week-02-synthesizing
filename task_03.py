0240796652793a96e35533cea08733e3e6f34d74
emad8878 <emad5223@gmail.com>
2015-09-07 08:30:23 +0000

    Adding week2 synthesizing homework @ github

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Hello This is week2 synthesizing homework task2"""
+
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
