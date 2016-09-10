cf16ec56bdf550c1e7a5bba1aa630d78aa738e2e
pinto921 <pinto921@hotmail.com>
2016-09-10 06:27:18 -1100

    Add files via upload

--- task_03.py
+++ task_03.py
@@ -1,7 +1,16 @@
-CHARLIE = "Chocolate Factory" 
+
+-CHARLIE = "chocolate factory"
++#!/usr/bin/env python
++# -*- coding: utf-8 -*-
++"""this is a docstring"""
++
++CHARLIE = "Chocolate Factory" 
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
+-
+-
+-
 
 
 
